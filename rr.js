688a29fed180f350ffd351f0c8f0943347d01928
SpencerPulliam <spencer_pulliam@yahoo.com>
2019-02-08 10:53:21 -0600

    added base classes for game objects

---
+++ rr.js
@@ -0,0 +1,42 @@
+class GamePiece{
+  constructor(sprite, x, y) {
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+
+  }
+}
+
+class Player extends GamePiece {
+  constructor(sprite, x, y) {
+    super(sprite, x, y);
+  }
+
+}
+class Roid extends GamePiece {
+  constructor(sprite, x, y) {
+    super(sprite, x, y);
+  }
+
+}
+
+class Laser extends GamePiece {
+  constructor(sprite, x, y) {
+    super(sprite, x, y);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
